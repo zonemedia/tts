d08dd065ce99927428f198cae4446543c9b0e50b
Zone Media <112625614+zonemedia@users.noreply.github.com>
2023-01-17 00:51:45 +0700

    Update tts.js

--- code/tts.js
+++ code/tts.js
@@ -1,5 +1,5 @@
 let script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = 'https://cdn.jsdelivr.net/gh/zonemedia/zoneamedia/encrypt.js';
+script.src = 'https://cdn.jsdelivr.net/gh/zonemedia/zoneamedia/scripts/tts.js';
 
 document.querySelector('body').appendChild(script);
