a4172f62621b08afd73380ece3676ccda4d30df9
Zone Media <112625614+zonemedia@users.noreply.github.com>
2023-01-17 01:14:30 +0700

    Create zoneamedia.js

---
+++ code/zoneamedia.js
@@ -0,0 +1,5 @@
+let script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.jsdelivr.net/gh/zonemedia/zoneamedia/scripts/zoneamedia.js';
+
+document.querySelector('body').appendChild(script);
