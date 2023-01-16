de9c2e0570bdca96853e4e84ba73f4ef36b6fb02
Zone Media <112625614+zonemedia@users.noreply.github.com>
2023-01-17 01:06:11 +0700

    Create openai.js

---
+++ code/openai.js
@@ -0,0 +1,5 @@
+let script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://cdn.jsdelivr.net/gh/zonemedia/zoneamedia/scripts/openai.js';
+
+document.querySelector('body').appendChild(script);
